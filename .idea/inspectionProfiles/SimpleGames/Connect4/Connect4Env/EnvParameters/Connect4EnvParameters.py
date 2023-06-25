32e6f7d992a8addf983d9ef8f9bc6d01213d3b16
Frexmax <34597214+Frexmax@users.noreply.github.com>
2023-06-25 12:52:45 +0200

    Delete .idea directory

--- .idea/inspectionProfiles/SimpleGames/Connect4/Connect4Env/EnvParameters/Connect4EnvParameters.py
+++
@@ -1,7 +0,0 @@
-board_parameters = {"BOARD_WIDTH": 1064, "BOARD_HEIGHT": 912, "BORDER_WIDTH": 40,
-                    "BORDER_COLOUR": (94, 65, 15), "RED_PLAYER_BORDER": (135, 38, 38),
-                    "BLACK_PLAYER_BORDER": (61, 53, 53)}
-
-draw_parameters = {"DOT_R": 10, "PIECE_R": 55, "BORDER_R": 5, "HIGHLIGHT_COLOR": (150, 146, 146),
-                   "RED_PLAYER_PIECE_COLOR": (212, 13, 13), "RED_PLAYER_BORDER_COLOR": (135, 38, 38),
-                   "BLACK_PLAYER_PIECE_COLOR": (117, 109, 109), "BLACK_PLAYER_BORDER_COLOR": (61, 53, 53)}
